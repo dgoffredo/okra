e10cd0285433ce7932a3e1663575455f60966170
David Goffredo <dmgoffredo@gmail.com>
2020-06-24 23:01:54 -0400

    think through some more schemas

--- schemas/crud.tisch.js
+++ schemas/crud.tisch.js
@@ -1,3 +1,63 @@
-// Each protobuf message has associated with it a set of operations for
-// creating, reading, updating, and deleting (CRUD) it representation in the
-// database. 
+// Each protobuf message has associated with it a set of procedures for
+// creating, reading, updating, and deleting (CRUD) its representation in the
+// database.
+//
+// Each procedure is composed of a sequence of operations for executing SQL
+// statements and unpacking the results into protocol buffer objects.
+//
+// This schema describes an operation.
+//
+or(
+    // Read-only SQL query. The `parameters` are input parameters.
+    {
+        'operation': 'query',
+        'sql': String,
+        'parameters': [String, ...etc] // protobuf field names
+    },
+
+    // Extract column values from the current result row, and advance to the
+    // next row.
+    {
+        'operation': 'read-row',
+        'destinations': [String, ...etc] // protobuf field names
+    },
+
+    // Extract the first column of all remaining rows, and append each value
+    // to the array-valued `destination`.
+    {
+        'operation': 'read-array',
+        'destination': String // protobuf field name
+    },
+
+    // Read/write SQL query. Not expected to produce any rows. The
+    // `parameters` are input parameters.
+    {
+        'operation': 'exec',
+        'sql': String,
+        'parameters': [String, ...etc] // protobuf field names
+    },
+
+    // Read/write SQL query. Not expected to produce any rows.
+    //
+    // This operation was designed for the case where you are inserting
+    // multiple rows into a table, e.g. to update an array-valued field. Such
+    // a SQL statement will look something like:
+    //
+    //     insert into boyscout_badges(boyscout_id, badge_id)
+    //     values (?, ?), (?, ?), (?, ?), (?, ?), ...
+    //
+    // where there's one "(?, ?)" per row, i.e. per value in the array-valued
+    // field. In an `exec-with-tuples` operation for the example above, the
+    // `tuple` is "(?, ?)" and the `sql` is everything before the first "(?,
+    // ?)". Exactly one of the `parameters` will be array-valued, and its
+    // length determines the number of copies of `tuple`. Other parameters are
+    // to be repeated for each element in the array-valued parameter (e.g. the
+    // `boyscout_id` is always the same, while the `badge_id` varies with the
+    // values in the array-valued parameter).
+    {
+        'operation': 'exec-with-tuples',
+        'tuple': String,
+        'sql': String,
+        'parameters': [String, ...etc] // protobuf field names
+    }
+)
--- schemas/table.tisch.js
+++ schemas/table.tisch.js
@@ -1,15 +1,37 @@
 // Each protobuf message and enum becomes its own SQL table, and each repeated
 // (array-valued) message field has its own SQL table (mapping the parent
-// object to its values in that field). This schema describes the JSON
-// representation of a JSON table.
+// object to its values for that field).
+//
+// This schema describes a JSON table.
+//
 ({
     'name': String,
     'columns': [{
         'name': String,
-        'protobuf_type': String,
+        // `protobuf_type` values are a subset of the names of the enum values
+        // in `enum FieldDescriptorProto.Type` in
+        // `google/protobuf/descriptor.proto`, which as of this writing is
+        // available on github.
+        // The types "TYPE_MESSAGE" and "TYPE_ENUM" are omitted -- messages
+        // aren't applicable, and enums are instead represented using
+        // "TYPE_INT32". Also, all of the "FIXED" integer types are omitted --
+        // their non-fixed equivalents are used instead. Finally, the explicitly
+        // signed (i.e. with an "S") integer types are omitted -- their plain
+        // equivalents are used instead (e.g. instead of "TYPE_SINT64", use
+        // "TYPE_INT64").
+        'protobuf_type': or(
+            'TYPE_DOUBLE',
+            'TYPE_FLOAT',
+            'TYPE_INT64',
+            'TYPE_UINT64',
+            'TYPE_INT32',
+            'TYPE_UINT32',
+            'TYPE_BOOL',
+            'TYPE_STRING',
+            'TYPE_BYTES'),
         'foreign_key?': {
-            'table': String,
-            'column': String
+            'table': String, // name of the foreign table
+            'column': String  // name of the column in the foreign table
         },
         'primary_key?': Boolean
     }, ...etc]
