e3a3a3d0c4eb5c01a9e10e2f01f5def9301b0e67
David Goffredo <davidg@okcupid.com>
2020-06-26 18:13:47 -0400

    tisch's define(...) works now

--- schemas/table.tisch.js
+++ schemas/table.tisch.js
@@ -4,38 +4,12 @@
 //
 // This schema describes a JSON table.
 //
-({
+define(['./builtin.tisch.js'], builtin => ({
     'name': String,
     'description?': String, // e.g. COMMENT section in MySQL
     'columns': [{
         'name': String,
-        // `protobuf_type` values are either a subset of the names of the enum values
-        // in `enum FieldDescriptorProto.Type` in
-        // `google/protobuf/descriptor.proto`, or the fully qualified protobuf
-        // type name of one of Google's "well-known types" (e.g.
-        // ".google.protobuf.Timestamp").
-        // A few things to note:
-        // - The types "TYPE_MESSAGE" and "TYPE_ENUM" are omitted. Messages
-        //   aren't applicable, and enums are instead represented using
-        //   "TYPE_INT32".
-        // - All of the "FIXED" integer types are omitted. Their non-fixed
-        //   equivalents are used instead (e.g. instead of "TYPE_FIXED64", use
-        //   "TYPE_UINT64").
-        // - The explicitly signed (i.e. with an "S") integer types are
-        //   omitted. Their plain equivalents are used instead (e.g. instead of
-        //   "TYPE_SINT64", use "TYPE_INT64").
-        // TODO: use 'protobuf_type': builtin
-        'protobuf_type': or(
-            'TYPE_DOUBLE',
-            'TYPE_FLOAT',
-            'TYPE_INT64',
-            'TYPE_UINT64',
-            'TYPE_INT32',
-            'TYPE_UINT32',
-            'TYPE_BOOL',
-            'TYPE_STRING',
-            'TYPE_BYTES',
-            '.google.protobuf.Timestamp'),
+        'protobuf_type': builtin,
         'foreign_key?': {
             'table': String, // name of the foreign table
             'column': String  // name of the column in the foreign table
@@ -43,4 +17,4 @@
         'primary_key?': Boolean,
         'description?': String // e.g. COMMENT section in MySQL
     }, ...etc]
-})
+}))
