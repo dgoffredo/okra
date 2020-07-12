b72814c412d6f115e286657a62dcc5e0d3ce9744
David Goffredo <dmgoffredo@gmail.com>
2020-07-12 00:07:21 -0400

    add schemas for database diffing

---
+++ schemas/alteration.tisch.js
@@ -0,0 +1,27 @@
+// "Alteration" as in the kinds of things you can do in a SQL "ALTER TABLE"
+// statement.
+define(['./builtin.tisch.js'], function (builtin) {
+    return or(
+        {
+            'kind': 'alter_column',
+            // There are two modifications that can be made to an existing column:
+            // - change to type, e.g. to widen an integer
+            // - change the documentation
+            'type?': builtin,
+            'description?': String // e.g. COMMENT section in MySQL
+        },
+        {
+            'kind': 'append_column',
+            // The rest of this is based on the definition of a column in
+            // `table.tisch.js`, except that "nullable" is assumed to be true,
+            // and "type" does not allow the value "name" (which is used only
+            // for the "name" column in enum tables).
+            'name': String,
+            'type': builtin,
+            'foreignKey?': {
+                'table': String, // name of the foreign table
+                'column': String  // name of the column in the foreign table
+            },
+            'description?': String // e.g. COMMENT section in MySQL
+        });
+})
---
+++ schemas/dbdiff.tisch.js
@@ -0,0 +1,57 @@
+// "dbdiff" stands for "database difference."
+//
+// Migrating the database schema from one version to another might involve:
+// - adding tables (e.g. a new type or a new array-valued field)
+// - adding columns to existing tables (e.g. new field in a message)
+// - modifying existing columns (e.g. expanding an int type or changing a comment)
+// - adding rows to existing tables (e.g. new enum values)
+// - modifying existing rows in tables (e.g. changing the description of an enum value)
+//
+// The set of all of those possiblities is described by this schema.
+define(['./table.tisch.js', './alteration.tisch.js'], function (table, alteration) {
+    return {
+        // All of the tables, new and old, modified and untouched.
+        // The keys of "all_tables" are the table names.
+        // This is here so that a SQL code generator can look up information
+        // about existing tables, e.g. the type of a primary key column.
+        'all_tables': {
+            [Any]: table,
+            ...etc
+        },
+
+        // Tables to add. The keys of "new_tables" are the table names.
+        'new_tables': {
+            [Any]: table,
+            ...etc
+        },
+
+        // Modifications to perform on existing tables. The keys of
+        // "modifications" are table names. The object at each key describes
+        // the modifications to make to the table whose name is that key.
+        'modifications': {
+            [Any]: {
+                // DDL operations, e.g. adding a column. See `alteration.tisch.js`.
+                'alterations': [alteration, ...etc],
+
+                // Rows to add. Each row is an array of values. The order of
+                // values in each row is the same as the order of the columns
+                // in the table definition.
+                'insertions': [Array, ...etc],
+
+                // Existing rows to update.
+                'updates': [{
+                    // value in the primary key column of the row to modify;
+                    // this is used in the INSERT statement's WHERE clause.
+                    'primaryKeyValue': or(Number, String),
+                    // columns to update, and which values to set them to;
+                    // each entry is a (column_name, new_value) pair.
+                    'column_values': {
+                        [Any]: or(Number, String, null),
+                        ...etc(1)
+                    }
+                }, ...etc]
+            },
+            ...etc
+        }
+    };
+})
