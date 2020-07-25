dac2c1fbfa299a5e534d94868d9fcc841be78c1f
David Goffredo <dmgoffredo@gmail.com>
2020-07-24 23:30:48 -0400

    TODO: js-beautify AST

--- crud-languages/go/render.test/breathing.json.js
+++ crud-languages/go/render.test/breathing.json.js
@@ -1,47 +1,66 @@
 ({
     package: 'main',
-    imports: [
-        {package: 'fmt'}
-    ],
-    functions: [
-        {name: 'doTheThing',
-          arguments: [
-              {name: 'whatThings', type: '[]string'},
-              {name: 'reallyThough', type: 'bool'}
-          ],
-          results: [],
-          body: {
-              variables: [
-                  {name: 'counter',
-                    type: 'int', 
+    imports: [{
+        package: 'fmt'
+    }],
+    functions: [{
+            name: 'doTheThing',
+            arguments: [{
+                    name: 'whatThings',
+                    type: '[]string'
+                },
+                {
+                    name: 'reallyThough',
+                    type: 'bool'
+                }
+            ],
+            results: [],
+            body: {
+                variables: [{
+                    name: 'counter',
+                    type: 'int',
                     value: {
                         call: {
                             function: 'len',
-                            arguments: [{symbol: 'whatThings'}]
-                        }}}
-              ],
-              statements: [
-                  {call: {
-                      function: {dot: ['fmt', 'Println']},
-                      arguments: ['the counter is at:', {symbol: 'counter'}]
-                  }}
-              ]
-          }},
+                            arguments: [{
+                                symbol: 'whatThings'
+                            }]
+                        }
+                    }
+                }],
+                statements: [{
+                    call: {
+                        function: {
+                            dot: ['fmt', 'Println']
+                        },
+                        arguments: ['the counter is at:', {
+                            symbol: 'counter'
+                        }]
+                    }
+                }]
+            }
+        },
 
-          {name: 'main',
-           arguments: [],
-           results: [],
-           body: {
-               variables: [],
-               statements: [
-                   {call: {
-                       function: 'doTheThing',
-                       arguments: [
-                           {slice: {type: '[]string', elements: ["foo", "bar"]}},
-                           true
-                       ]
-                   }}
-               ]
-           }}
+        {
+            name: 'main',
+            arguments: [],
+            results: [],
+            body: {
+                variables: [],
+                statements: [{
+                    call: {
+                        function: 'doTheThing',
+                        arguments: [{
+                                slice: {
+                                    type: '[]string',
+                                    elements: ["foo", "bar"]
+                                }
+                            },
+                            true
+                        ]
+                    }
+                }]
+            }
+        }
     ]
 })
