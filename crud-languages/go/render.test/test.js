86ef18aaaad5e74c03666addd24f5ab3a41e76e1
David Goffredo <dmgoffredo@gmail.com>
2020-09-20 19:52:41 -0400

    clean up another test

--- crud-languages/go/render.test/test.js
+++ crud-languages/go/render.test/test.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const path = require('path');
 const vm = require('vm');
 const {renderFile} = require('../render');
-const {glob} = require('../../../lib/filesystem');
+const {glob, diff} = require('../../../lib/filesystem');
 
 // Here's how this test driver works:
 // - Each *.json.js file is an input (JS representation of Go source file AST)
@@ -25,12 +25,14 @@ astPaths.forEach(astPath => {
         fs.readFileSync(astPath, {encoding: 'utf8'}));
 
     const goSource = renderFile(ast);
-    // TODO: This is not yet a test.
-    // I'll want to write a diff function, which I'll want a tempfile function
-    // for...
+    const stem = path.basename(astPath, '.json.js');
+    const expectedPath = stem + '.go';
 
-    // TODO: This isn't a unit test yet, we just print things. It's still
-    // test-like, though, in that we're testing whether an exception is thrown. So,
-    // I comment out the log line below as not to spam the output of`bin/test`.
-    // console.log(goSource);
+    const diffResult = diff({path: expectedPath}, {string: goSource});
+    if (diffResult.length !== 0) {
+        throw Error(`Expected Go ${expectedPath} and generated Go differ:\n${diffResult}`);
+    }
 });
+
+// If we got here, then nothing above threw, so we succeeded.
+console.log(`All ${astPaths.length} tests passed.`);
